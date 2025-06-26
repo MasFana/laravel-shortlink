8f79dc61589c29e33c80d5cc625b1103f4fbe033
Phisict <adrianfathan69@gmail.com>
2025-06-27 04:08:12 +0700

    Feat : Resp

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
+// use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
         
         $ListLinkCode = [
             [
